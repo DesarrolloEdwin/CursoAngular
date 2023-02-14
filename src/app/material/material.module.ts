008dbb6a22b5d9104e1d291deeab249656320500
Edwin Leonel <desarrollo.ti7@tucafi.com>
2023-02-14 17:33:36 -0600

    se agregaron funcionalidades para el sidebard

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,13 +3,15 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   exports:[
     MatButtonModule,
     MatIconModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule
   ]
 })
 export class MaterialModule { }
